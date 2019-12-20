dd6caf75bdbdf1279a2d5b7906cda6f1ade4370e
Marcus Pöhls <marcus.poehls@gmail.com>
2019-12-20 11:45:50 +0100

    use @supercharge/hapi-aws-lambda

--- server.js
+++ server.js
@@ -1,44 +1,15 @@
 'use strict'
 
-const Querystring = require('querystring')
 const App = require('@supercharge/framework/application')
+const HapiOnLambda = require('@supercharge/hapi-aws-lambda')
+
+let handler
 
 module.exports.handler = async event => {
-  const app = await new App().fromAppRoot(__dirname).httpForLambda()
-
-  const { server } = app
-  const querystring = Querystring.stringify(event.multiValueQueryStringParameters)
-
-  const { statusCode, rawPayload, headers } = await server.inject({
-    method: event.httpMethod,
-    url: querystring ? `${event.path}?${querystring}` : event.path,
-    payload: event.isBase64Encoded ? Buffer.from(event.body, 'base64') : event.body,
-    headers: Object.entries(event.multiValueHeaders || {})
-      .reduce((collect, [name, value]) => ({
-        ...collect,
-        [name]: (value.length === 1) ? value[0] : value
-      }), {})
-  })
-
-  // chunked transfer not currently supported by API Gateway
-  if (headers['transfer-encoding'] === 'chunked') {
-    delete headers['transfer-encoding']
+  if (!handler) {
+    const { server } = await App.fromAppRoot(__dirname).httpForServerless()
+    handler = HapiOnLambda.for(server)
   }
 
-  const { 'content-type': type, 'content-encoding': encoding } = headers
-  const isBase64Encoded = Boolean(type && !type.match(/; *charset=/)) || Boolean(encoding && encoding !== 'identity')
-
-  const finalHeaders = Object
-    .entries(headers)
-    .reduce((collect, [name, value]) => ({
-      ...collect,
-      [name]: [].concat(value)
-    }), {})
-
-  return {
-    statusCode,
-    isBase64Encoded,
-    body: rawPayload.toString(isBase64Encoded ? 'base64' : 'utf8'),
-    multiValueHeaders: finalHeaders
-  }
+  return handler.proxy(event)
 }
